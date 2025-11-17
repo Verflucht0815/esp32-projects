c04d6ba854b10fa10e524b1590cd0c33e4f8120d
NPC0815 <chriskreativeoase@gmail.com>
2025-11-17 04:19:41 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Bastelwelt - JavaScript
-// Einfache Version - TikTok Embeds werden direkt in HTML eingefügt
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Bastelwelt geladen - bereit für DIY Projekte!');
-    
-    // Hier ist keine zusätzliche Logik mehr nötig
-    // Die TikTok Videos werden direkt in der index.html eingefügt
-});
